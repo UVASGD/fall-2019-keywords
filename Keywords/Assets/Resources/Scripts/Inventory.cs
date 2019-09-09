b9883ea7980b71876d73c2c5293c5678b461838e
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2019-09-09 14:35:02 -0400

    Inventory Scaling Better

--- Keywords/Assets/Resources/Scripts/Inventory.cs
+++ Keywords/Assets/Resources/Scripts/Inventory.cs
@@ -58,7 +58,12 @@ public class Inventory : MonoBehaviour {
         Transform SlotUI = UI.transform.Find("Slot" + activeSlot);
         GameObject ItemInUI = Instantiate(obj, SlotUI.position, Quaternion.identity, SlotUI);
         Game.RepositionHeight(ItemInUI, Height.UI);
-        ItemInUI.transform.localScale = new Vector3(UIScaleFactor, UIScaleFactor, ItemInUI.transform.localScale.z);
+        bool scaleToWidth = (ItemInUI.transform.localScale.x >= ItemInUI.transform.localScale.y);
+        if (scaleToWidth) {
+            ItemInUI.transform.localScale = new Vector3(UIScaleFactor, UIScaleFactor * (ItemInUI.transform.localScale.y / ItemInUI.transform.localScale.x), ItemInUI.transform.localScale.z);
+        } else {
+            ItemInUI.transform.localScale = new Vector3(UIScaleFactor * (ItemInUI.transform.localScale.x / ItemInUI.transform.localScale.y), UIScaleFactor, ItemInUI.transform.localScale.z);
+        }
         Game.SetLayer(ItemInUI, LayerMask.NameToLayer("P" + GetComponent<PlayerInfo>().playerNum));
         Game.DisablePhysics(ItemInUI);
     }
