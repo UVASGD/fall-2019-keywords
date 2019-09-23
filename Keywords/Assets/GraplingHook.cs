ce88d3da92707837aabe480aaa8a65e4d45bf602
wjm6ukw <54868186+wjm6ukw@users.noreply.github.com>
2019-09-22 21:04:37 -0400

    added basics of grappling hook

---
+++ Keywords/Assets/GraplingHook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraplingHook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Vector2 launchDirection = Vector2.left;
+    public float launchSpeed;
+    public float pullSpeed;
+
+    void LaunchHook()
+    {
+        
+
+
+
+    }
+
+    void PullPlayer()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
