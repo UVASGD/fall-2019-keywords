6bc2b8c1097da8799b3f8e7579332c2baeb9d099
ryanakann <ryanakann@gmail.com>
2019-09-23 12:53:49 -0400

    GameManager reference updates

---
+++ Keywords/Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+
+	public static Words words;
+	public static MakeWalls makeWalls;
+	public static Quit quit;
+
+	private void Awake () {
+		if (instance) {
+			Destroy(gameObject);
+		} else {
+			instance = this;
+		}
+
+		words = GetComponent<Words>();
+		makeWalls = GetComponent<MakeWalls>();
+		quit = GetComponent<Quit>();
+	}
+}
