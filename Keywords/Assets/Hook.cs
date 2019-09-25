cbf8b1dfb52cfbb62e4a9c180267336633ced724
wjm6ukw <54868186+wjm6ukw@users.noreply.github.com>
2019-09-25 14:31:51 -0400

    Basic Functionality
    Grappling hook performs basic functions,
    Still Required:
    Ability to set distance
    ability to manually disable
    ability to pickup (currently spawns on player)

--- Keywords/Assets/Hook.cs
+++ Keywords/Assets/Hook.cs
@@ -6,21 +6,29 @@ public class Hook : MonoBehaviour
 {
     // Start is called before the first frame update
     public Rigidbody2D rb;
-    public float maxLength;
-    bool hasFired;
+    float maxLength = 3f;
+    public bool hasFired;
     float curLength;
     public SpringJoint2D spring;
-    bool hasLanded;
+    public bool hasHit, hasReturned, toReturn;
+    float maxReturnTime = 5f;
 
-    GameObject player, grapplingHook;
+    public GameObject player, grapplingHook;
+
+    Grabbable target;
 
 
      public void launch(Vector2 dir, float speed)
     {
-
         if (hasFired)
             return;
+
+        print("Launching");
+        transform.parent = null;
+        rb.bodyType = RigidbodyType2D.Dynamic;
         hasFired = true;
+        hasReturned = false;
+        hasHit = false;
         rb.velocity = dir * speed;
         curLength = 0;
         spring.enabled = false;
@@ -28,6 +36,13 @@ public class Hook : MonoBehaviour
 
     public void launchReturn()///for when we run out of rope/time and hook is sent back to player
     {
+        if (hasReturned)
+        {
+            StopAllCoroutines();
+            return;
+        }
+        toReturn = true;
+        print("Returning launch");
         //change physics type to dynamic
         rb.bodyType = RigidbodyType2D.Dynamic;
         //change anchor to gun
@@ -37,60 +52,103 @@ public class Hook : MonoBehaviour
     }
     public void pullPlayer()
     {
+        print("Pulling player");
         //change physics to kinematic
         rb.bodyType = RigidbodyType2D.Kinematic;
         //change anchor to player
         spring.connectedBody = player.GetComponent<Rigidbody2D>();
         //enable spring
         spring.enabled = true;
+        StartCoroutine(ReturnTime());
+    }
+
+    public void grab(Grabbable target)
+    {
+        print("Grabbing: " + target);
+        target.grab(rb);
+        spring.connectedBody = grapplingHook.GetComponent<Rigidbody2D>();
+        spring.enabled = true;
+        this.target = target;
+        launchReturn();
+        StartCoroutine(ReturnTime());
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        print("Triggered: " + collision.gameObject.name + " ree");
         if (hasFired)
         {
-            if (hasLanded)
+            if(collision.gameObject == player || collision.gameObject == grapplingHook)
             {
-                if(collision.gameObject == player || collision.gameObject == grapplingHook)
+                if (hasHit || toReturn)
                 {
+                    print("Collided with home: " + collision.gameObject);
                     hasFired = false;
+                    hasReturned = true;
                     rb.velocity = Vector2.zero;
                     curLength = 0;
                     spring.enabled = false;
+                    toReturn = false;
+                    if (target)
+                        this.target.ungrab();
+                    transform.position = grapplingHook.transform.position;
+                    transform.parent = grapplingHook.transform;
+                    rb.bodyType = RigidbodyType2D.Kinematic;
                     // allow the gun to be fired again
                 }
             }
-            else
+            else if (!hasHit)
             {
-                //GetComponent<Grabbable>, pullPlayer
-                hasLanded = true;
-                rb.velocity = Vector2.zero;
-                curLength = 0;
-                spring.enabled = false;
+                if (collision.GetComponent<Grabbable>())
+                {
+                    hasHit = true;
+                    rb.velocity = Vector2.zero;
+                    curLength = 0;
+                    grab(collision.GetComponent<Grabbable>());
+                }
+                else if (collision.CompareTag("Wall"))
+                {
+                    hasHit = true;
+                    rb.velocity = Vector2.zero;
+                    curLength = 0;
+                    pullPlayer();
+                }
             }
         }
     }
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         spring = GetComponent<SpringJoint2D>();
+        spring.enabled = false;
     }
 
     public void Setup(GameObject player, GameObject grapplingHook)
     {
+        print("Set up");
         this.player = player;
         this.grapplingHook = grapplingHook;
+        spring.connectedBody = grapplingHook.GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    IEnumerator ReturnTime()
+    {
+        yield return new WaitForSeconds(maxReturnTime);
+        print("We've reached return time");
+        launchReturn();
+        if (target)
+            this.target.ungrab();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (hasFired && !hasLanded)
+        if (hasFired && !hasHit)
         {
             if (curLength < maxLength)
             {
-                curLength += Time.deltaTime;
-                
+                curLength += Time.deltaTime;          
             }
             else
             {
