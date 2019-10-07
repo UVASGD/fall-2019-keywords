6e8bad5e583b9211deb2a14df4c4d732659b16a9
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2019-10-07 00:26:06 -0400

    get rid of old text files, randomize seed

--- Keywords/Assets/Resources/Scripts/Seed.cs
+++ Keywords/Assets/Resources/Scripts/Seed.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class Seed : MonoBehaviour
 {
     public int seed;
+    public bool randomSeed;
     private void Awake() {
+        if (randomSeed)
+        {
+            seed = Random.Range(1, 100000000);
+        }
         Random.InitState(seed);
     }
 }
