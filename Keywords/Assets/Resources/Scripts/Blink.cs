4ac04239fee04c80c7ee9f617570366957bb3b96
Jeffrey-Wang <17jwapples@gmail.com>
2019-10-27 22:04:08 -0400

    blink now (mostly) matches specs

--- Keywords/Assets/Resources/Scripts/Blink.cs
+++ Keywords/Assets/Resources/Scripts/Blink.cs
@@ -7,9 +7,21 @@ public class Blink : Fireable {
     [SerializeField]
     [Range(1000f, 5000f)]
     private float DASH_SPEED = 3000f;
+    private float DISTANCE = 1.2f;
+    private LayerMask WALL_LAYER_MASK;
+
+    void Start() {
+        WALL_LAYER_MASK = LayerMask.GetMask("Wall");
+    }
 
     public override void Fire(Vector2 v, GameObject firingPlayer) {
         Rigidbody2D rb = firingPlayer.GetComponent<Rigidbody2D>();
-        rb.AddForce(v * DASH_SPEED);
+        Vector2 dest = rb.position + v * DISTANCE;
+        RaycastHit2D raycast = Physics2D.CircleCast(dest, 0.05f, Vector2.up, 0.01f, WALL_LAYER_MASK);
+        if (raycast.collider == null) {
+            rb.position = dest;
+        } else {
+            // Debug.Log("Blink denied by " + raycast.collider.gameObject.name);
+        }
     }
 }
