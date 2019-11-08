7466bc04bc3fd3068879102035ae5c6daffbc373
Nathaniel Saxe <NathanielSaxophone@gmail.com>
2019-11-08 14:31:57 -0500

    cooldown class

---
+++ Keywords/Assets/Resources/Scripts/ParentClasses/Cooldown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cooldown {
+    float cooldown;
+    float timeStarted;
+
+    public Cooldown(float myCooldown) {
+        cooldown = myCooldown;
+        timeStarted = -cooldown - 1f;
+    }
+
+    public bool Ready() {
+        return Time.time - timeStarted >= cooldown;
+    }
+
+    public void Reset() {
+        timeStarted = Time.time;
+    }
+
+    public void SetCooldown(float newCooldown) {
+        cooldown = newCooldown;
+    }
+}
