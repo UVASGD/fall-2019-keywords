b2e63ea69bb163b5e42b5d48964e2ad2f498dda1
Wyatt Joyner <wyatt.joyner@practicalcode.us>
2019-11-10 20:55:20 -0500

    oracle added

--- Keywords/Assets/Resources/Scripts/Machines/Oracle.cs
+++ Keywords/Assets/Resources/Scripts/Machines/Oracle.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Oracle : Machine {
+    [HideInInspector]
     public Words words = GameManager.words;
+    WordDisplayer word_displayer;
+
+    private void Awake()
+    {
+        word_displayer = GetComponentInChildren<WordDisplayer>();
+    }
 
     protected override void PerformMachineAction() {
         //delete tile
@@ -12,5 +19,6 @@ public class Oracle : Machine {
         slot.GetComponent<GridSquare>().tile = null;
         //TODO: make piece of paper object with this word instead of printing
         print(words.GetRandomUnmadeWord());
+        word_displayer.DisplayWord(words.GetRandomUnmadeWord());
     }
 }
