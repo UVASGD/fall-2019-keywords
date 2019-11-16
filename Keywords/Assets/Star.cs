f64e3d6217905e0a67ce16ccd8de3988e5973c19
Justin Becker <jab7yp@virginia.edu>
2019-11-15 23:51:17 -0500

    Star Changes

--- Keywords/Assets/Star.cs
+++ Keywords/Assets/Star.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class Star : MonoBehaviour
 {
-    float timeCounter = 0;
+    float timeCounter;
     // Start is called before the first frame update
     void Start()
     {
-
-        if(gameObject.name == "Star1")
+        timeCounter = 0;
+        if (gameObject.name == "Star1")
         {
-            timeCounter = 160;
+            timeCounter = 90;
         }
         if (gameObject.name == "Star2")
         {
-            timeCounter = 250;
+            timeCounter = 180;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        Circle(transform.parent.position);
     }
 
     public void Circle(Vector2 pos)
     {
-            timeCounter += .3f;
+            timeCounter +=  .1f ;
             float x = Mathf.Cos(timeCounter) * .1f;
             float y = Mathf.Sin(timeCounter) * .1f;
             transform.position = new Vector2(x + pos.x, y + pos.y + .12f);
