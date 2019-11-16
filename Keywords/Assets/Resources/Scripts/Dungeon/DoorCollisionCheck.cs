effa959dccee7a437b7e0a6a243db571c67c100a
ryanakann <ryanakann@gmail.com>
2019-11-15 23:29:54 -0500

    Door indicator implemented

--- Keywords/Assets/Resources/Scripts/Dungeon/DoorCollisionCheck.cs
+++ Keywords/Assets/Resources/Scripts/Dungeon/DoorCollisionCheck.cs
@@ -30,9 +30,16 @@ public class DoorCollisionCheck : MonoBehaviour {
         foreach (Transform child in doors) {
             Door door = child.gameObject.GetComponent<Door>();
             if (player.keys >= door.keyNum) {
-                //				print ("Ayy");
                 door.Unlock(playerNum);
                 Physics2D.IgnoreCollision(player.gameObject.GetComponent<CircleCollider2D>(), door.GetComponent<BoxCollider2D>());
+                
+                // Create indicator that goes towards the door when it unlocks. If it's close or it's mid/late game
+                if (player.keys > 13 || (door.transform.position - player.transform.position).magnitude < 15f) {
+                    GameObject adoorable = Instantiate(Resources.Load("Prefabs/FX/GlowingOrbFX"), player.transform.position, Quaternion.identity) as GameObject;
+                    adoorable.GetComponent<GoToDoor>().GoTo(door.transform);
+                    string layerName = "P" + playerNum.ToString();
+                    adoorable.layer = LayerMask.NameToLayer(layerName);
+                }
             } else {
                 //				print ("yyA");
                 Physics2D.IgnoreCollision(player.gameObject.GetComponent<CircleCollider2D>(), door.GetComponent<BoxCollider2D>(), false);
