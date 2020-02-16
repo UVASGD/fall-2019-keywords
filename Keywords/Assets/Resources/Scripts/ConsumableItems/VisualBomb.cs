52571e1c9936683d4f71fcf766ce8cefbf9fc127
AidanHimley <55163434+AidanHimley@users.noreply.github.com>
2020-02-16 01:09:52 -0500

    Added timer to visual bomb
    Bomb detonates 20s after being placed, timer resets upon taking the bomb. No place sound effect yet, currently plays detonate twice.

--- Keywords/Assets/Resources/Scripts/ConsumableItems/VisualBomb.cs
+++ Keywords/Assets/Resources/Scripts/ConsumableItems/VisualBomb.cs
@@ -5,15 +5,37 @@ using UnityEngine;
 public class VisualBomb : Placeable {
     public GameObject indicator;
     AudioSource SwapBombSFX;
+    private bool onGrid = false;
+    public float fuseTime = 20f;
+    private Cooldown fuse;
+    private GridControl gc;
+
     public override void PlaceOn(GameObject square, GameObject placingPlayer) {
         SwapBombSFX = GameManager.instance.sfx["SwapBombSFX"];
         base.PlaceOn(square, placingPlayer);
-        GridControl gc = square.transform.parent.gameObject.GetComponent<GridControl>();
-        if (gc) {
+        onGrid = true;
+        gc = square.transform.parent.gameObject.GetComponent<GridControl>();
+        if (fuse == null) {
+            fuse = new Cooldown(fuseTime);
+        }
+        fuse.Start();
+        SwapBombSFX.Play();
+    }
+
+    public override void TakeFrom(GameObject square, GameObject takingPlayer)
+    {
+        onGrid = false;
+        base.TakeFrom(square, takingPlayer);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (onGrid && fuse.Ready() && gc) {
             Explode(gc);
             Destroy(gameObject);
         }
     }
+
     void Explode(GridControl grid) {
         SwapBombSFX.Play();
         int cipher = Random.Range(1, 26);
